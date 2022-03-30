45adf9c30e41c3e98d1b21cc0aefb2f289eca63f
Ricardo Augusto <ricardoaugusto.eb@gmail.com>
2022-03-30 10:51:54 -0300

    complete example

--- main.dart
+++ main.dart
@@ -1,4 +1,18 @@
 // TYPE NAME(PARAMS){}
 main() {
-  print("HELLO WORLD");
+  // Instância da classe
+  var minhaClasse = MinhaClasse();
+
+  printHelloWorld(message: minhaClasse.message);
+}
+
+// Criada a função, pode chamar ela na função main
+void printHelloWorld({required String message}) {
+  print(message.replaceAll("DEU", "NÃO DEU"));
+}
+
+// class NomeDaClasse{}
+
+class MinhaClasse {
+  var message = "DEU CERTO";
 }
